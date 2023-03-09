e41aa87c1a6c02e4d57d0d7af9dded1a19e03640
AnujChhikara <anujchhikara777@gmail.com>
2023-03-09 20:41:26 +0530

    height

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 import NewExpense from "./components/NewExpense/NewExpense";
-import ExpensesFilter from "./components/ExpenseFilter";
 import { useState } from "react";
 
 function App() {
-  const [filteredYear, setFilteredYear] = useState("2023");
   const [data, setData] = useState([]);
-
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
 
   const Items = data.map(function (item, index) {
     return (
@@ -28,7 +22,7 @@ function App() {
   };
 
   return (
-    <div className="bg-[#222222] h-screen  text-white">
+    <div className=" text-white">
       <div className="flex pt-3 underline-offset-[3px] text-White font-bold  text-3xl justify-center">
         Expense Tracker
       </div>
@@ -36,13 +30,7 @@ function App() {
         <NewExpense onAddExpense={addExpenseHandler} />
       </div>
 
-      <div className="mt-4 mx-2 space-y-3 ">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {Items}
-      </div>
+      <div className="mt-4 mx-2 space-y-3 ">{Items}</div>
     </div>
   );
 }
